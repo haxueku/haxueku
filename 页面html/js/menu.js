fb6a4f73804e719139dc5daa393167b6dc8af06a
zzb9004 <403676825@qq.com>
2016-03-10 17:59:24 +0800

    123
    Signed-off-by: zzb9004 <403676825@qq.com>

---
+++ 页面html/js/menu.js
@@ -0,0 +1,36 @@
+$(function(){
+    $(".ce > li > a").click(function(){
+		 $(this).children('i').addClass('ce_jian').removeClass('ce_jia').parents().siblings().find(".ce_jian").addClass('ce_jia').removeClass('ce_jian');
+	     $(this).addClass("xz").parents().siblings().find("a").removeClass("xz");
+		 $(this).parents().siblings().find(".er").hide();
+		 $(this).siblings(".er").toggle();
+		 $(this).parents().siblings().find(".er > li > .thr").hide().parents().siblings().find(".thr_nr").hide();
+	})
+	
+    $(".er > li > a").click(function(){
+		$(this).children('i').addClass('ce_jian').removeClass('ce_jia').siblings('i').addClass('ce_jia').removeClass('ce_jian');
+        $(this).addClass("sen_x").parents().siblings().find("a").removeClass("sen_x");
+        $(this).parents().siblings().find(".thr").hide();	
+	    $(this).siblings(".thr").toggle();	
+	})
+
+    $(".thr > li > a").click(function(){
+	     $(this).addClass("xuan").parents().siblings().find("a").removeClass("xuan");
+		 $(this).parents().siblings().find(".thr_nr").hide();	
+	     $(this).siblings(".thr_nr").toggle();
+	})
+	$(".j_c").click(function(){
+		$('.j_c').addClass("na_n_check").removeClass("na_n");
+		$('.zsd').addClass("na_n").removeClass("na_n_check");
+		$('.j_c_list').show();
+		$('.zsd_list').hide();
+  });
+  $(".zsd").click(function(){
+		$('.zsd').addClass("na_n_check").removeClass("na_n");
+		$('.j_c').addClass("na_n").removeClass("na_n_check");
+		$('.j_c_list').hide();
+		$('.zsd_list').show();
+  });
+});
+
+  
